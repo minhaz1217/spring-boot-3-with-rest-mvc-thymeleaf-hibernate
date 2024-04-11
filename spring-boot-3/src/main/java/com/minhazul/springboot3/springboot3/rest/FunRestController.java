e70184152c16f64a01c3a75f1f99bc9346a53888
Minhaz <minhaz1217@gmail.com>
2024-04-11 23:29:16 +0600

    19. devtools integration working

--- spring-boot-3/src/main/java/com/minhazul/springboot3/springboot3/rest/FunRestController.java
+++ spring-boot-3/src/main/java/com/minhazul/springboot3/springboot3/rest/FunRestController.java
@@ -10,6 +10,18 @@ public class FunRestController {
     private String developerName;
     @GetMapping("/")
     public String sayHello(){
-        return "HELLO " + developerName;
+        return "HELLO 2 " + developerName;
     }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
+    }
+
+    // expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is a lukcy day";
+    }
+
 }
