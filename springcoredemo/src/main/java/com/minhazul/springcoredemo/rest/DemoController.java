f1ced0e42d322cd38e8db384eb9daa03e43563d9
Minhaz <minhaz1217@gmail.com>
2024-04-13 00:32:56 +0600

    50. using primary for dependency injection

--- springcoredemo/src/main/java/com/minhazul/springcoredemo/rest/DemoController.java
+++ springcoredemo/src/main/java/com/minhazul/springcoredemo/rest/DemoController.java
@@ -11,7 +11,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController(@Qualifier("tennisCoach") Coach theCoach){
+    public DemoController(Coach theCoach){
         myCoach = theCoach;
     }
     @GetMapping("/dailyworkout")
