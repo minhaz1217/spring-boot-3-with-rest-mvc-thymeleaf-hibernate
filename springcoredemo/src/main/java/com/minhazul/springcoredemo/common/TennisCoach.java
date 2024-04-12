714e5b287cf21230dd89eeab76aa0ae33d8613c8
Minhaz <minhaz1217@gmail.com>
2024-04-13 00:45:31 +0600

    55. bean scope prototype or singleton

--- springcoredemo/src/main/java/com/minhazul/springcoredemo/common/TennisCoach.java
+++ springcoredemo/src/main/java/com/minhazul/springcoredemo/common/TennisCoach.java
@@ -1,12 +1,15 @@
 package com.minhazul.springcoredemo.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
 @Lazy
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class TennisCoach implements  Coach{
     public TennisCoach(){
         System.out.println("Constructor of: " + getClass().getSimpleName());
--- springcoredemo/src/main/java/com/minhazul/springcoredemo/rest/DemoController.java
+++ springcoredemo/src/main/java/com/minhazul/springcoredemo/rest/DemoController.java
@@ -11,13 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     private Coach myCoach;
+    private Coach _secondCoach;
     @Autowired
-    public DemoController(Coach theCoach){
+    public DemoController(Coach theCoach, Coach secondCoach){
         System.out.println("Constructor of: " + getClass().getSimpleName());
         myCoach = theCoach;
+        _secondCoach = secondCoach;
     }
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/compare-coach")
+    public String compareCoach(){
+        return "myCoach == _secondCoach " + (myCoach == _secondCoach ? "True" : "False");
     }
 }
