f6014c0a379706c4b3d358683c0bad5ae7eab119
Minhaz <minhaz1217@gmail.com>
2024-04-13 00:35:45 +0600

    53. lazy initialization

--- springcoredemo/src/main/java/com/minhazul/springcoredemo/common/TennisCoach.java
+++ springcoredemo/src/main/java/com/minhazul/springcoredemo/common/TennisCoach.java
@@ -1,11 +1,16 @@
 package com.minhazul.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Lazy
 public class TennisCoach implements  Coach{
+    public TennisCoach(){
+        System.out.println("Constructor of: " + getClass().getSimpleName());
+    }
     @Override
     public String getDailyWorkout() {
         return "Practice Tennis for 15 minutes.";
--- springcoredemo/src/main/java/com/minhazul/springcoredemo/rest/DemoController.java
+++ springcoredemo/src/main/java/com/minhazul/springcoredemo/rest/DemoController.java
@@ -3,15 +3,17 @@ package com.minhazul.springcoredemo.rest;
 import com.minhazul.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Lazy
 @RestController
 public class DemoController {
     private Coach myCoach;
-
     @Autowired
     public DemoController(Coach theCoach){
+        System.out.println("Constructor of: " + getClass().getSimpleName());
         myCoach = theCoach;
     }
     @GetMapping("/dailyworkout")
