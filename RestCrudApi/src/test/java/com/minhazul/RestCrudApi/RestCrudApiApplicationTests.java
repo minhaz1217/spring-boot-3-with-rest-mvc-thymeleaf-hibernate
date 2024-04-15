67e8e10f32ba8302f5ee6b8ffe879d89d7d6da1b
Minhaz <minhaz1217@gmail.com>
2024-04-15 23:29:32 +0600

    98. setting up base project

---
+++ RestCrudApi/src/test/java/com/minhazul/RestCrudApi/RestCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhazul.RestCrudApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
