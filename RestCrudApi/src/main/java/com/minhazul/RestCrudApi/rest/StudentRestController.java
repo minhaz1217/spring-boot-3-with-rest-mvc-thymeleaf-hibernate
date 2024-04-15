115209e2d3920e88f765174d14e2861cc18be793
Minhaz <minhaz1217@gmail.com>
2024-04-15 23:44:06 +0600

    105. reading path variable

--- RestCrudApi/src/main/java/com/minhazul/RestCrudApi/rest/StudentRestController.java
+++ RestCrudApi/src/main/java/com/minhazul/RestCrudApi/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.minhazul.RestCrudApi.rest;
 
 import com.minhazul.RestCrudApi.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +13,21 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<>();
         students.add((new Student("Minhazul", "Khan")));
         students.add((new Student("Hayat", "Khan")));
         students.add((new Student("Anik", "Khan")));
-
+    }
+    @GetMapping("/students")
+    public List<Student> getStudents(){
         return students;
+    }
+    
+    @GetMapping("/student/{studentId}")
+    public Student getStudentById(@PathVariable int studentId){
+        return students.get(studentId);
     }
 }
