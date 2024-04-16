98308f23480463bcd96b80e79351401c5ca78ddc
Minhaz <minhaz1217@gmail.com>
2024-04-16 22:06:41 +0600

    113. using controller advice for handling exception

--- RestCrudApi/src/main/java/com/minhazul/RestCrudApi/rest/StudentRestController.java
+++ RestCrudApi/src/main/java/com/minhazul/RestCrudApi/rest/StudentRestController.java
@@ -32,22 +32,4 @@ public class StudentRestController {
         }
         return students.get(studentId);
     }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exception){
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exception.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exception){
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exception.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
