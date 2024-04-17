264f695e2e0e62963901d40c2e82a07ffcb0659e
Minhaz <minhaz1217@gmail.com>
2024-04-17 23:37:17 +0600

    118. project init

---
+++ CRUDWithDB/src/main/java/com/minhazul/CRUDWithDB/CrudWithDbApplication.java
@@ -0,0 +1,13 @@
+package com.minhazul.CRUDWithDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWithDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWithDbApplication.class, args);
+	}
+
+}
---
+++ CRUDWithDB/src/test/java/com/minhazul/CRUDWithDB/CrudWithDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhazul.CRUDWithDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWithDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
