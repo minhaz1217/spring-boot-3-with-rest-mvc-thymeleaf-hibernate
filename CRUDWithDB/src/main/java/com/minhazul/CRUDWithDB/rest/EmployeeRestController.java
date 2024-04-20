d0b5782a3c6c7bed35e81d41a3e77ae97b729a24
Minhaz <minhaz1217@gmail.com>
2024-04-21 00:19:14 +0600

    131. called all the services from controller

--- CRUDWithDB/src/main/java/com/minhazul/CRUDWithDB/rest/EmployeeRestController.java
+++ CRUDWithDB/src/main/java/com/minhazul/CRUDWithDB/rest/EmployeeRestController.java
@@ -3,9 +3,7 @@ package com.minhazul.CRUDWithDB.rest;
 import com.minhazul.CRUDWithDB.dao.EmployeeDAO;
 import com.minhazul.CRUDWithDB.entity.Employee;
 import com.minhazul.CRUDWithDB.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +20,42 @@ public class EmployeeRestController{
     public List<Employee> findAll(){
         return _employeeService.findAll();
     }
+
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee employee = _employeeService.findById(employeeId);
+        if(employee == null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee){
+        employee.setId(0);
+
+        Employee newEmployee = _employeeService.save(employee);
+        return newEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        Employee updatedEmployee = _employeeService.save(employee);
+        return updatedEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        Employee employee = _employeeService.findById(employeeId);
+        if(employee == null){
+            throw new RuntimeException("Employee not found - " + employeeId);
+        }
+        _employeeService.deleteById(employeeId);
+        return "Deleted - " + employeeId;
+    }
+
+
+
+
 }
--- CRUDWithDB/src/main/java/com/minhazul/CRUDWithDB/service/EmployeeService.java
+++ CRUDWithDB/src/main/java/com/minhazul/CRUDWithDB/service/EmployeeService.java
@@ -13,4 +13,3 @@ public interface EmployeeService {
 
     void deleteById(int id);
 }
-}
